6754c7f3abcc3d17a3c857f99e4eff8121624527
DuyguY <duygu.yldrm4820@gmail.com>
2022-01-04 12:06:33 -0500

    addd tables and datas

--- 3_queries_group-by/3_queries_group-by.sql
+++ 3_queries_group-by/3_queries_group-by.sql
@@ -33,13 +33,21 @@
 -- GROUP BY cohorts.name 
 -- ORDER BY COUNT (assignment_submissions.assignment_id) DESC;
 
-SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration, AVG(assignments.duration) as average_estimated_duration 
-FROM assignment_submissions 
-JOIN students
-ON student_id =students.id
-JOIN assignments
-ON assignment_id =assignments.id
-WHERE students.end_date IS NULL
-GROUP BY students.name 
-HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
-ORDER BY average_assignment_duration;
+-- SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration, AVG(assignments.duration) as average_estimated_duration 
+-- FROM assignment_submissions 
+-- JOIN students
+-- ON student_id =students.id
+-- JOIN assignments
+-- ON assignment_id =assignments.id
+-- WHERE students.end_date IS NULL
+-- GROUP BY students.name 
+-- HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
+-- ORDER BY average_assignment_duration;
+
+SELECT (
+  SELECT count(assignments)
+  FROM assignments
+)-count(assignment_submissions) as total_incomplete
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.name = 'Ibrahim Schimmel';
