b77572ca5259cb1d7330c05f6ba4ecc5cc5768ba
DuyguY <duygu.yldrm4820@gmail.com>
2022-01-05 23:30:00 -0500

    Set up the JavaScript project

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,2 +1,18 @@
-const pg = require('pg');
-const Client = pg.Client;
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`)
+.then(res => {
+  console.log(res);
+})
+.catch(err => console.error('query error', err.stack));
